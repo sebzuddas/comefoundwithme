93ebb59a9f9ba5796dcbf2e2e6ed9d79c783b96f
sebzuddas <sebastianozuddas1@gmail.com>
2025-07-20 17:16:31 +0200

    many changes to ui and content

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,183 +1,85 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+// Navigation data for the site
+// All external links should open in a new tab
+
+// Social links used in both header and footer
+export const socialLinks = [
+  { 
+    text: 'Twitter', 
+    href: 'https://www.x.com/sebzuddas', 
+    icon: 'tabler:brand-x',
+    ariaLabel: 'Twitter'
+  },
+  { 
+    text: 'LinkedIn', 
+    href: 'https://www.linkedin.com/in/sebzuddas/', 
+    icon: 'tabler:brand-linkedin',
+    ariaLabel: 'LinkedIn'
+  },
+  { 
+    text: 'Instagram', 
+    href: 'https://www.instagram.com/sebzuddas', 
+    icon: 'tabler:brand-instagram',
+    ariaLabel: 'Instagram'
+  },
+  { 
+    text: 'GitHub', 
+    href: 'https://github.com/sebzuddas', 
+    icon: 'tabler:brand-github',
+    ariaLabel: 'GitHub'
+  },
+];
 
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Me',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'Website',
+          href: 'https://www.sebzuddas.com',
+          target: '_blank',
+          icon: 'tabler:link'
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+          text: 'Projects',
+          href: 'https://www.sebzuddas.com/projects',
+          target: '_blank',
+          icon: 'tabler:folder'
+        }
+      ]
     },
     {
-      text: 'Pages',
+      text: 'Get in touch',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'Email',
+          href: 'mailto:sebastianozuddas1@gmail.com',
+          icon: 'tabler:mail'
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: 'WhatsApp',
+          href: 'https://wa.me/+447598064088',
+          icon: 'tabler:brand-whatsapp',
+          target: '_blank'
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
+          text: 'Calendly',
+          href: 'https://calendly.com/sebastianozuddas1/30min',
+          icon: 'tabler:form-input',
+          target: '_blank'
+        }
+      ]
+    }
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [],
+  socialLinks: socialLinks,
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://www.x.com/sebzuddas' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/sebzuddas/' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/sebzuddas' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/sebzuddas' },
   ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
 };
