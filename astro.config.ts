fb277f625f673aacb547f307cd8266f4367a08b1
sebzuddas <sebastianozuddas1@gmail.com>
2025-07-20 13:16:50 +0200

    more changes

--- astro.config.ts
+++ astro.config.ts
@@ -22,12 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://sebzuddas.github.io',
-  base: '/comefoundwithme',
+  site: 'https://sebzuddas.github.io/comefoundwithme',
   output: 'static',
-  build: {
-    assets: '_astro',
-  },
 
   integrations: [
     tailwind({
