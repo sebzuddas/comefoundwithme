e5304b9c92c7eebe8b4e051ecec6fce277829bc5
sebzuddas <sebastianozuddas1@gmail.com>
2025-07-20 13:51:14 +0200

    pushing to subdomain instead

--- astro.config.ts
+++ astro.config.ts
@@ -22,8 +22,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://sebzuddas.com',
-  base: '/comefoundwithme',
+  site: 'https://found.sebzuddas.com',
   output: 'static',
 
   integrations: [
